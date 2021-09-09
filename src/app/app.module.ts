adb2a54bec72e7405fa3dc401f59faff021cf876
garrettruss <garrettmruss@gmail.com>
2021-09-09 15:18:45 -0400

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Dinner at the Wharf',
+    day: 'September 15th at 6:00pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Infy Meeting',
+    day: 'September 10 at 11am',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Tuxedo Shopping',
+    day: 'October 7 at 12:30pm',
+    reminder: false,
+  },
+];
