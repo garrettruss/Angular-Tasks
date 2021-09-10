4da48079bd73947d0fe83b77908b6c14ec2ba1e7
garrettruss <garrettmruss@gmail.com>
2021-09-09 21:25:21 -0400

    show add task

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'angular-tasks';
+  showAddTask: boolean = false;
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private uiService:UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+   }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    console.log('toggle')
+    this.uiService.toggleAddTask();
   }
 
 }
